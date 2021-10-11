0a29c0218b9e5099992a083ec3eb9c428ce6ccdc
Anderson Vera <synapse.unit@gmail.com>
2021-10-11 13:38:31 -0500

    React Hooks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Characters from './components/Characters';
+
+function App() {
+  return (
+    <div className="App">
+    <Header></Header>
+    <Characters></Characters>
+    </div>
+  );
+}
+
+export default App;
